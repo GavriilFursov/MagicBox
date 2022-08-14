4625dbe90e76e1658260fe04431c6ea28d5e3985
GavriilFursov <From.skveki1@mail.ru>
2022-08-14 15:10:18 +0300

    Adding java files

---
+++ src/MagicBox.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class Box<T> {
+    protected int size;
+    protected T[] items;
+
+    public Box(int size) {
+        this.size = size;
+        items = (T[]) new Object[size];
+    }
+
+    Random random = new Random();
+    int filled = 0;
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+
+                filled += 1;
+                System.out.println(Arrays.toString(items));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (filled < items.length) {
+                throw new RuntimeException("свободных ячеек: " + (items.length - filled) + ", их надо заполнить.");
+            }
+            if (filled == items.length) {
+                int randomInt = random.nextInt(items.length);
+                System.out.println("коробка заполнена.");
+                System.out.println("случайный элемент из коробки: " + items[randomInt]);
+                System.out.println();
+                return items[randomInt];
+            }
+        }
+        return null;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Box<String> box1 = new Box<>(2);
+        Box<Integer> box2 = new Box<>(3);
+
+        box1.add("table");
+        box1.add("chair");
+        box1.pick();
+        box2.add(15);
+        box2.add(45);
+        box2.add(4);
+        box2.pick();
+    }
+}
